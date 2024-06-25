4364d70b5f92caa09df50dabcd18a5e5684ee659
DipSapkota125 <dipsapkota325@gmail.com>
2024-06-25 09:18:05 +0545

    Added Variables

---
+++ index.js
@@ -0,0 +1,61 @@
+//Variables
+//var
+// var x = 90;
+// console.log(x);
+// var x = 100;
+// console.log(x);
+
+//let
+// let y = 600;
+// console.log(y);
+
+// y = 4500;
+// console.log(y);
+
+//const
+// const result = 90;
+// console.log(result);
+
+// result = 67;
+// console.log(result);
+
+//Data Types in JavaScript
+//String
+// const output = "Ram Thapa";
+// console.log(output);
+// console.log(typeof output);
+
+//Number
+// const result = 90;
+// console.log(result);
+// console.log(typeof result);
+
+//float
+// const x = 3.56;
+// console.log(x);
+// console.log(typeof x);
+
+//undefined
+// let y;
+// console.log(y);
+// console.log(typeof y);
+
+//null
+// const x = null;
+// console.log(x);
+// console.log(typeof x);
+
+//Boolean
+// const isTrue = true;
+// console.log(isTrue);
+// console.log(typeof isTrue);
+
+//BigInt
+// const result = 34663778965n;
+// console.log(result);
+// console.log(typeof result);
+
+//Symbol
+const output = Symbol("Hello World");
+console.log(output);
+console.log(typeof output);
